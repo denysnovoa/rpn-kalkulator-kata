10d5740937330ee93a52f171ea7959919ce281e5
denysnovoa <dnovoa20@gmail.com>
2017-06-22 16:48:04 +0200

    make_operation_with_multiple_digits_and_operator

--- src/main/kotlin/RpnKalkulator.kt
+++ src/main/kotlin/RpnKalkulator.kt
@@ -2,22 +2,28 @@ class RpnKalkulator {
     fun calcule(i: String): Int {
         val input = i.split(" ")
         val arrayNumbers = arrayListOf<Int>()
-        var operator = ""
+        val operators = arrayListOf<String>()
 
         input.forEach {
             try {
                 val value = Integer.parseInt(it)
-
                 arrayNumbers.add(value)
             } catch (ex: NumberFormatException) {
-                operator = it
+                val resultOperation = when (it) {
+                    "/" -> arrayNumbers[0].div(arrayNumbers[1])
+                    "-" -> arrayNumbers[0].minus(arrayNumbers[1])
+                    "+" -> arrayNumbers[arrayNumbers.size-2].plus(arrayNumbers[arrayNumbers.size-1])
+                    "*" -> arrayNumbers[arrayNumbers.size-1].times(arrayNumbers[arrayNumbers.size-2])
+                    else -> arrayNumbers[0]
+                }
+
+                arrayNumbers.remove(arrayNumbers.last())
+                arrayNumbers.remove(arrayNumbers.last())
+                arrayNumbers.add(resultOperation)
             }
         }
 
-       return when (operator) {
-            "/" -> arrayNumbers[0].div(arrayNumbers[1])
-            else -> arrayNumbers[0]
-        }
+        return arrayNumbers[0]
     }
 }
 
--- src/test/kotlin/RpnKalkulatorShould.kt
+++ src/test/kotlin/RpnKalkulatorShould.kt
@@ -26,6 +26,14 @@ class RpnKalkulatorShould {
         Assert.assertTrue(3 == rpnKalkualtor.calcule("4 2 + 3 -"))
     }
 
+    @Test
+    fun make_operation_with_multiple_digits_and_operator() {
+
+        val rpnKalkualtor = RpnKalkulator()
+
+        Assert.assertTrue(141 == rpnKalkualtor.calcule("3 5 8 * 7 + *"))
+    }
+
     @Before
     fun setUp() {
     }
