6df783b8b5558fb26d43f60cc1eec2a67fef1487
denysnovoa <dnovoa20@gmail.com>
2017-06-22 16:17:10 +0200

    make_operation_with_three_digits

--- src/test/kotlin/RpnKalkulatorShould.kt
+++ src/test/kotlin/RpnKalkulatorShould.kt
@@ -18,6 +18,14 @@ class RpnKalkulatorShould {
         Assert.assertTrue(4 == rpnKalkualtor.calcule("20 5 /"))
     }
 
+    @Test
+    fun make_operation_with_three_digits() {
+
+        val rpnKalkualtor = RpnKalkulator()
+
+        Assert.assertTrue(3 == rpnKalkualtor.calcule("4 2 + 3 -"))
+    }
+
     @Before
     fun setUp() {
     }
